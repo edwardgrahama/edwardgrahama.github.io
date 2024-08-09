62a9865cb17bb108df8a6c7c82bd444606e5488a
edwardgrahama <80621936+edwardgrahama@users.noreply.github.com>
2024-08-09 18:19:15 +0530

    Update fetch-bid-values.js

--- script/fetch-bid-values.js
+++ script/fetch-bid-values.js
@@ -2,7 +2,7 @@
 document.body.innerHTML = `<div id='data'></div><form id="urlForm">
 <label for="urls">URLs (separated by commas or new lines):</label><br>
 <textarea id="urls" rows="10" cols="50" placeholder="Enter URLs here..."></textarea><br><br>
-<button type="button" onclick="extractLid()">Get Bid Values</button>
+<button type="button" onclick="extractLid()">Get Bid Values</button><span id='progress'></span>
 </form><div id='main'></div><script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"></script>`
 
 DIV = document.getElementById('main')
@@ -16,6 +16,7 @@ DIV.appendChild(TABLE)
 //auctionID = 264335
 
 var dat
+var miss=0;
 async function getData(lotid) {
 
    await fetch("https://jjkane.proxibid.com/asp/xml/LotTimeRemAllXML.asp?aid=0", {
@@ -25,7 +26,29 @@ async function getData(lotid) {
    })
       .then(response => response.text())
       .then(str => new window.DOMParser().parseFromString(str, "text/html"))
-      .then(data => { document.body.firstChild.innerHTML = data.body.firstChild.innerHTML; dat =data ;tableDat(data) });
+      .then(data => { document.body.firstChild.innerHTML = data.body.firstChild.innerHTML; 
+         dat =data ;
+         tableDat(data);
+
+         if(data.body.firstChild.firstChild.children.length == 0){
+            console.log("Couldn\'t retrieve data. Invalid or Broken URL")
+            console.log("Check URL associated with item no "+parseInt(cnt+1))
+            console.log("Jumping to next item")
+            miss++
+            cnt++
+         }
+         else{
+            
+            document.getElementById('progress').innerHTML = 'Retrieving data '+parseInt(cnt+1-miss)+' of '+arr.length+' URLs' 
+            cnt++
+         }
+         if(cnt == arr.length){
+            document.getElementById('progress').innerHTML = "Completed. "+ parseInt(cnt-miss)+" out of "+cnt+" data retrieved"
+         }
+         else{
+            getData(arr[cnt])
+         }
+      });
 
 }
 
@@ -54,11 +77,11 @@ function generateTableHead() {  //generate table head
 
 }
 
-function tableDat() {
-   itemData = dat.body.firstChild.firstChild
-   lot = itemData.children[6].innerText
-   price = itemData.children[4].innerText
-   timeRem = itemData.children[0].innerText
+function tableDat(data) {
+   itemData = data.body.firstChild.firstChild
+   lot = itemData.children[6]
+   price = itemData.children[4]
+   timeRem = itemData.children[0]
 
    let row = tbody.insertRow()
    let text;
@@ -67,38 +90,38 @@ function tableDat() {
 
    cell = row.insertCell()
    if (lot) {
-      text = document.createTextNode(lot)
-
+      text = document.createTextNode(lot.innerText)
+      console.log(parseInt(cnt+1)+". "+lot.innerText)
    }
    else {
-      text = document.createTextNode('')
+      text = document.createTextNode('Item no. ')
    }
    cell.appendChild(text)
    cell.innerHTML +='<br>'
 
    cell = row.insertCell()
    if (price) {
-      text = document.createTextNode(price)
+      text = document.createTextNode(price.innerText)
    }
    else {
-      text = document.createTextNode('')
+      text = document.createTextNode(cnt+1)
    }
    cell.appendChild(text)
    cell.innerHTML +='<br>'
 
    cell = row.insertCell()
    if (timeRem) {
-      res = ConvertSectoDay(parseInt(timeRem))
+      res = ConvertSectoDay(parseInt(timeRem.innerText))
       text = document.createTextNode(res)
    }
    else {
-      text = document.createTextNode('')
+      text = document.createTextNode(' Invalid URL')
    }
    cell.appendChild(text)
    cell.innerHTML +='<br>'
    row.innerHTML +='<br>'
 
-   console.log(lot)
+  
 }
 
 function ConvertSectoDay(n) {
@@ -112,10 +135,11 @@ function ConvertSectoDay(n) {
       day + " " + "days " + hour + " " + "hours "
    );
 }
-
+var cnt = 0;
 
 var arr;
 function extractLid() {
+   reset()
    const input = document.getElementById('urls').value;
    const urlArray = input.split(/\s+|,/).filter(url => url.trim() !== '');
    const extractedArray = urlArray.map(url => {
@@ -125,9 +149,19 @@ function extractLid() {
    console.log(extractedArray)
    arr = extractedArray
 
-   for (i = 0; i < arr.length; i++) {
-      getData(arr[i])
-   }
+   //for (i = 0; i < arr.length; i++) {
+
+      getData(arr[cnt])
+  // }
+
+}
+
+
+
+function reset(){
+   tbody.innerHTML = ''
+   //document.getElementById("data-output").firstChild.innerHTML = ''
+   cnt = 0
 }
 
 generateTableHead()
